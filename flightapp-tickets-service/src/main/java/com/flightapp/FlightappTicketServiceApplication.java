5533a3f719080a4a7e17e6a41284fb7a6d19ef2f
sureshcskkumar <sureshcskkumar@gmail.com>
2022-07-07 16:00:36 +0530

    Intermediate commit

--- flightapp-tickets-service/src/main/java/com/flightapp/FlightappTicketServiceApplication.java
+++ flightapp-tickets-service/src/main/java/com/flightapp/FlightappTicketServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class FlightappUserserviceApplication {
+public class FlightappTicketServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightappUserserviceApplication.class, args);
+		SpringApplication.run(FlightappTicketServiceApplication.class, args);
 	}
 
 }
--- flightapp-tickets-service/src/main/java/com/flightapp/controller/TicketController.java
+++ flightapp-tickets-service/src/main/java/com/flightapp/controller/TicketController.java
@@ -18,7 +18,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/tickets")
 public class TicketController {
 
 	@Autowired
